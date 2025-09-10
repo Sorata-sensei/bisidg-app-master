117b5002a9f114a4a7b2c8aa36dd65e028c74e74
Sora <52536164+Sorata-sensei@users.noreply.github.com>
2025-09-10 13:42:07 +0700

    repair text warning

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -49,9 +49,11 @@
                         <img src="{{ asset('storage/' . $user->ttd) }}" alt="ttd {{ $user->name }}" class="img-thumbnail"
                             style="max-height: 120px;">
                     @endif
-                    <p class="text-white btn-danger">Mohon pastikan TTD tidak ada background nya atau bisa gunakan ini untuk
+                    <p class="text-white btn btn-danger">Mohon pastikan TTD tidak ada background nya atau bisa gunakan ini
+                        untuk
                         menghilangkan background sebelum menggunggah <a
-                            href="https://www.photoroom.com/tools/background-remover">Background Remover</a></p>
+                            href="https://www.photoroom.com/tools/background-remover" class="text-primary">Background
+                            Remover</a></p>
                 </div>
             </div>
             <div class="mb-3 row">
